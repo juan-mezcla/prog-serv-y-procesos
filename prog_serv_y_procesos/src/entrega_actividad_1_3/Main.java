bb74decd50c1ff3d6b78a4ef3daa8260c5e15577
alumno alumno@DAM206 <juanmezcua96@gmail.com>
2025-10-14 14:15:53 +0200

    Añadido algo de comentarios. terminar para hacer javadoc

--- prog_serv_y_procesos/src/entrega_actividad_1_3/Main.java
+++ prog_serv_y_procesos/src/entrega_actividad_1_3/Main.java
@@ -11,39 +11,42 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		
+
 		try {
-			
-			Process suma=new ProcessBuilder("java","entrega_actividad_1_3.Suma").start();
-			BufferedWriter escribir=new BufferedWriter(new OutputStreamWriter(suma.getOutputStream()));
-			if(args.length>=2) {
-				
-				escribir.write(args[0]+"\n");
+
+			Process suma = new ProcessBuilder("java", "entrega_actividad_1_3.Suma").start();
+			// Creamos la variable escribir para mandar los datos al hijo.
+			BufferedWriter escribir = new BufferedWriter(new OutputStreamWriter(suma.getOutputStream()));
+
+			// Comprobamos que haya metido en terminal al hacer el comando java dos
+			// parametros.
+			if (args.length >= 2) {
+
+				escribir.write(args[0] + "\n");
 				escribir.write(args[1]);
-				
+
 				escribir.flush();
 				escribir.close();
-			}else {
-				throw new java.lang.ArrayIndexOutOfBoundsException(); 
+			} else {// Throw en caso de no añadir los datos.
+				throw new java.lang.ArrayIndexOutOfBoundsException();
 			}
-			
-			
-			BufferedReader leer=new BufferedReader(new InputStreamReader(suma.getInputStream()));
-			
+
+			// Leemos los datos que ha procesado suma.
+			BufferedReader leer = new BufferedReader(new InputStreamReader(suma.getInputStream()));
+
 			String linea;
-			
-			while((linea=leer.readLine())!=null) {
+
+			while ((linea = leer.readLine()) != null) {
 				System.out.println(linea);
 			}
 			leer.close();
-			
-			
-			BufferedReader leerError=new BufferedReader(new InputStreamReader(suma.getErrorStream()));
-			
+
+			// Leemos los datos en caso de error de suma.
+			BufferedReader leerError = new BufferedReader(new InputStreamReader(suma.getErrorStream()));
+
 			String lineaError;
-			
-			while((lineaError=leerError.readLine())!=null) {
+
+			while ((lineaError = leerError.readLine()) != null) {
 				System.out.println(lineaError);
 			}
 			leerError.close();
@@ -54,11 +57,11 @@ public class Main {
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}catch (java.lang.ArrayIndexOutOfBoundsException e) {
+		} catch (java.lang.ArrayIndexOutOfBoundsException e) {
 			// TODO Auto-generated catch block
 			System.out.println("Tienes que poner 2 numeros para que funcione el programa.");
 		}
-		
+
 	}
 
 }
--- prog_serv_y_procesos/src/entrega_actividad_1_3/Suma.java
+++ prog_serv_y_procesos/src/entrega_actividad_1_3/Suma.java
@@ -9,31 +9,30 @@ public class Suma {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		try {
-			BufferedReader leer=new BufferedReader(new InputStreamReader(System.in));	
-		int n1=0;
-		int n2=0;
-		//System.out.println("Valores de n1 y n2:"+n1+", "+n2);
-		n1 = Integer.parseInt(leer.readLine().trim());
-		
-		n2=Integer.parseInt(leer.readLine().trim());
-		
-		//System.out.println("Valores de n1 y n2:"+n1+", "+n2);
-		int total=n1+n2;
-		
-		
-		System.out.println("El total de la suma entre "+n1+" y "+n2+" es : "+total);
-		
-		System.exit(0);
-		}catch(java.lang.NumberFormatException e) {
+			// leemos los datos que nos han pasado.
+			BufferedReader leer = new BufferedReader(new InputStreamReader(System.in));
+			int n1 = 0;
+			int n2 = 0;
+			// System.out.println("Valores de n1 y n2:"+n1+", "+n2);
+			n1 = Integer.parseInt(leer.readLine().trim());
+
+			n2 = Integer.parseInt(leer.readLine().trim());
+
+			// System.out.println("Valores de n1 y n2:"+n1+", "+n2);
+			int total = n1 + n2;
+
+			System.out.println("El total de la suma entre " + n1 + " y " + n2 + " es : " + total);
+
+			System.exit(0);// programa acabado con exito.
+		} catch (java.lang.NumberFormatException e) {
 			System.out.println("No es un numero lo que has introducido.");
-			System.exit(1);
-		}catch (IOException e) {
+			System.exit(1);// error.
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-			System.exit(1);
+			System.exit(1);// error.
 		}
-		
-		
+
 	}
 
 }
